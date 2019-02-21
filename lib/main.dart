d68fb2a79391c2ebc48b1e510d2d1816b556ba0c
jessvilches <vilchesjessica@hotmail.com>
2019-02-21 16:54:25 -0500

    white

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
 
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.white,
       ),
       home: new LoginPage(),
     );
