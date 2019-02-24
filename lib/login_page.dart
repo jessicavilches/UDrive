2773e6ab796c318d8f11be17b4d35a601d2692f1
martinfriedmann <mxf361@miami.edu>
2019-02-24 16:44:28 -0500

    Login test

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,11 +7,18 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
+  final formKey = new GlobalKey<FormState>();
   String _email;
   String _password;
 
   void validateAndSave() {
+    final form = formKey.currentState;
+    if (form.validate()){
+      form.save();
+      print('Form is valid. Email: $_email, password: $_password');
+    } else {
+      print('Form is invalid');
+    }
 
   }
 
@@ -24,17 +31,20 @@ class _LoginPageState extends State<LoginPage> {
       body: Container(
         padding: EdgeInsets.all(16.0),
         child: new Form(
+          key: formKey,
           child: new Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children:<Widget>[
               new TextFormField(
                 decoration: new InputDecoration(labelText: 'Email'),
                 validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+                onSaved: (value) => _email = value,
               ),
               new TextFormField(
                 decoration: new InputDecoration(labelText: 'Password'),
-                validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
                 obscureText: true,
+                validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+                onSaved: (value) => _password = value,
               ),
               new RaisedButton(
                 child: new Text('Login',style: new TextStyle(fontSize: 20.0)),
