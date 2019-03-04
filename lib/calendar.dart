fd56b3f23c85449cd0e152fabee59aa3634e5427
martinfriedmann <mxf361@miami.edu>
2019-03-04 13:38:05 -0500

    Mode workingg

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -7,16 +7,18 @@ class Calendar extends StatefulWidget{
 
 class _Calendar extends State<Calendar>{
     DateTime _date = new DateTime.now();
-    TimeOfDay _time = new TimeOfDay.now();
+    TimeOfDay _startTime = new TimeOfDay.now();
+    TimeOfDay _endTime = new TimeOfDay.now();
+    String _address;
 
-    Future<Null> _selectDate(BuildContext context) async{
+    void _selectDate(BuildContext context) async{
       final DateTime picked = await showDatePicker(
           context: context,
           initialDate: _date,
           firstDate: new DateTime(DateTime.now().year),
           lastDate: new DateTime(2020)
       );
-      if(picked != null && picked != _date){
+      if(picked != null){
         print('Date selected:  ${_date.toString()}');
         setState(() {
           _date = picked;
@@ -24,18 +26,63 @@ class _Calendar extends State<Calendar>{
       }
     }
 
+    Future<Null> _selectStartInterval(BuildContext context) async{
+      final DateTime picked = (await showTimePicker(
+          context: context,
+          initialTime: _startTime
+      )) as DateTime;
+      if(picked != null && picked != _date){
+        print('Time selected:  ${_startTime.toString()}');
+        setState(() {
+          _startTime = picked as TimeOfDay;
+        });
+      }
+    }
+
+    Future<Null> _selectEndInterval(BuildContext context) async{
+      final DateTime picked = (await showTimePicker(
+          context: context,
+          initialTime: _endTime
+      )) as DateTime;
+      if(picked != null && picked != _date){
+        print('Time selected:  ${_endTime.toString()}');
+        setState(() {
+          _endTime = picked as TimeOfDay;
+        });
+      }
+    }
+
     @override
     Widget build(BuildContext context){
-      print('Here');
       return new Scaffold(
         body: new Container(
           padding: new EdgeInsets.all(32.0),
           child: new Column(
             children: <Widget>[
-              new Text ('Date selected: ${_date.toString()}'),
+              Align(
+                alignment: Alignment.centerLeft,
+              ),
               new RaisedButton(
                 child: new Text('Select Date'),
-                onPressed: (){_selectDate(context);}
+                onPressed: (){
+                  _selectDate(context);
+                  },
+              ),
+              new RaisedButton(
+                child: new Text('Select Start Time'),
+                onPressed: (){_selectStartInterval(context);},
+              ),
+              new RaisedButton(
+                child: new Text('Select End Time'),
+                onPressed: (){_selectEndInterval(context);},
+              ),
+              new TextFormField(
+                decoration: new InputDecoration(labelText: 'Address'),
+                validator: (value) => value.isEmpty ? 'Address can\'t be empty' : null,
+              ),
+              new RaisedButton(
+                child: new Text('Submit Ride'),
+                onPressed: (){_selectDate(context);},
               ),
             ],
           ),
