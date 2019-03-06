62e1875eb56e9317625963414b9d0c6cfce12984
Greg <gxp174@miami.edu>
2019-03-05 22:17:32 -0500

    -had db rides printed to feed page with date and first address

--- lib/feed.dart
+++ lib/feed.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'globals.dart' as globals;
 import 'services/crud.dart';
+import 'dart:async';
 
 class Feed extends StatefulWidget{
   @override
@@ -8,179 +10,73 @@ class Feed extends StatefulWidget{
 }
 
 class _Feed extends State<Feed> {
-  final formKey = new GlobalKey<FormState>();
-  DateTime _date = new DateTime.now();
-  TimeOfDay _startTime = new TimeOfDay.now();
-  TimeOfDay _endTime = new TimeOfDay.now();
-  String _startAddress;
-  String _endAddress;
-  crudMethods crudObj = new crudMethods();
-
-
-  Widget _buildContent() {
-    return ListView.builder(
-      //itemCount: allContacts.length,
-        itemBuilder: (BuildContext content, int index) {
-          // Contact contact = allContacts[index];
-          // return ContactListTile(contact);
-        });
-  }
-
-
   @override
-  void initstate() {
-    crudObj.getData().then((results) {
-
-    });
-  }
-
-/*
-  class ContactListTile extends ListTile {
-  ContactListTile(Contact contact)
-      : super(
-  title: Text(contact.name),
-  subtitle: Text(contact.email),
-  leading: CircleAvatar(child: Text(contact.name[0])),
-  );
-  }*/
-
-  bool validateAndSave() {
-    final form = formKey.currentState;
-    if (form.validate()) {
-      form.save();
-      return true;
-    }
-    return false;
-  }
-
-  void _selectDate(BuildContext context) async {
-    final DateTime picked = await showDatePicker(
-        context: context,
-        initialDate: _date,
-        firstDate: new DateTime(DateTime
-            .now()
-            .year),
-        lastDate: new DateTime(2020)
+  Widget build(BuildContext context) {
+    return Container(
+    //return new Scaffold(
+     // appBar: new AppBar(
+       //   title: new Text('Pool Feed')
+     // ),
+      child: ListPage()
+    //);
     );
-    if (picked != null) {
-      setState(() {
-        _date = picked;
-      });
-      print(' selected:  ${_date.toString()}');
-    }
   }
+}
+class ListPage extends StatefulWidget {
+  @override
+  _ListPageState createState() => _ListPageState();
+}
 
-  Future<Null> _selectStartInterval(BuildContext context) async {
-    final TimeOfDay picked = (await showTimePicker(
-        context: context,
-        initialTime: _startTime
-    )); //as DateTime;
-    if (picked != null) {
-      setState(() {
-        _startTime = picked;
-      });
-      print('Time selected:  ${_startTime.toString()}');
-    }
-  }
+class _ListPageState extends State<ListPage> {
 
-  Future<Null> _selectEndInterval(BuildContext context) async {
-    final TimeOfDay picked = (await showTimePicker(
-        context: context,
-        initialTime: _endTime
-    ));
-    if (picked != null) {
-      setState(() {
-        _endTime = picked;
-      });
-      print('Time selected:  ${_endTime.toString()}');
-    }
+  Future getPosts() async {
+    var firestore = Firestore.instance;
+    QuerySnapshot qn = await firestore.collection('Rides').getDocuments();
+    for(int i = 0; i < qn.documents.length;i++)
+      {
+        print(qn.documents[i].data["date"]);
+      }
+    return qn.documents;
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Container(
-          padding: EdgeInsets.all(32.0),
-          child: new Form(
-            key: formKey,
-            child: new ListView(
-              children: <Widget>[
-                SizedBox(height: 100),
-                new RaisedButton(
-                  child: new Text('FEED: ' + this._date.month.toString()
-                      + '/' + this._date.day.toString() + '/' +
-                      this._date.year.toString(),
-                      style: new TextStyle(fontSize: 20.0)),
-                  onPressed: () {
-                    _selectDate(context);
-                  },
-                ),
-                SizedBox(height: 20),
-                new RaisedButton(
-                  child: new Text(
-                      'Select Start Time: ' + this._startTime.format(context),
-                      style: new TextStyle(fontSize: 20.0)),
-                  onPressed: () {
-                    _selectStartInterval(context);
-                  },
-                ),
-                SizedBox(height: 20),
-                new RaisedButton(
-                  child: new Text(
-                      'Select End Time: ' + this._endTime.format(context),
-                      style: new TextStyle(fontSize: 20.0)),
-                  onPressed: () {
-                    _selectEndInterval(context);
-                  },
-                ),
-                SizedBox(height: 20),
-                new TextFormField(
-                  decoration: new InputDecoration(labelText: 'Start Address'),
-                  validator: (value) =>
-                  value.isEmpty
-                      ? 'Start Address can\'t be empty'
-                      : null,
-                  onSaved: (value) => _startAddress = value,
-                ),
-                new TextFormField(
-                  decoration: new InputDecoration(labelText: 'End Address'),
-                  validator: (value) =>
-                  value.isEmpty
-                      ? 'Start Address can\'t be empty'
-                      : null,
-                  onSaved: (value) => _endAddress = value,
-                ),
-                SizedBox(height: 40),
-                new RaisedButton(
-                  child: new Text(
-                      'Submit Ride', style: new TextStyle(fontSize: 20.0)),
-                  onPressed: () { //_selectDate(context);
-                    addToDatabase();
-                  },
-                ),
-              ],
-            ),
-          )
-      ),
+    return Container(
+      child: FutureBuilder(
+          future: getPosts(),
+          builder: (_,snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: Text('Loading...'),
+              );
+            } else {
+              return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (_, index) {
+
+                    return ListTile(
+                      title: Text(snapshot.data[index].data["date"]), //sub in fname, lname etc
+                      subtitle: Text(snapshot.data[index].data["start_address"]),
+                      //subtitle: Text(snapshot.data[index].data["end_address"]),
+
+                    );
+                  });
+            }
+          }),
     );
   }
+}
 
-
-  void addToDatabase() async {
-    if (validateAndSave()) {
-      Map <String, dynamic> rideData = {
-        'date': this._date.toString(),
-        'start_time': this._startTime.toString(),
-        'end_time': this._endTime.toString(),
-        'start_address': this._startAddress,
-        'end_address': this._endAddress,
-        'uid' : globals.get_userID()
-      };
-      crudObj.addRide(rideData).catchError((e) {
-        print(e);
-      });
-    }
-    // moveToLogin(); This should clear all values and let you submit a new ride
-    //}
+class DetailPage extends StatefulWidget {
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+class _DetailPageState extends State<DetailPage>{
+  @override
+  Widget build(BuildContext context){
+    return Container();
   }
 }
+
+
+ 
